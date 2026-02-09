9c488f43767886d2f97a03d2567ddf8c519a315d
Jagesh Madhaiyan <jagesh02mk@gmail.com>
2026-02-09 15:26:36 +0530

    Compatible safe alternative cors fix

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
+// SAFE OPTIONS handler
+app.use((req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
