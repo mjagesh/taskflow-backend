1d6d52c8aa9d305c794bfe112b7f9dd77f58e431
Jagesh Madhaiyan <jagesh02mk@gmail.com>
2026-02-09 14:42:11 +0530

    cors code changes

--- server.js
+++ server.js
@@ -13,7 +13,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://taskflow-frontend-neon.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
